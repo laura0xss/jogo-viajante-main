1c5b111e2ab661f71936269987b23cb2327f112a
laura0xss <laura.0xss@gmail.com>
2026-09-04 20:29:43 -0300

    enviAR

--- scripit.js
+++ scripit.js
@@ -41,33 +41,35 @@ function mostrarResultado() {
     let mensagens = {
 
         Steven: `
+            <img src="img/1.png" alt="Steven Universe" style="width: 200px; height: auto;">'
             <h2>💗 Você é o Steven!</h2>
             <p>Você é carinhoso, empático e sempre tenta ajudar quem está ao seu redor. 
             Você acredita que as pessoas podem resolver seus problemas juntas!</p>
         `,
 
         Amethyst: `
+            <img src="img/2.png" alt="Amethyst" style="width: 200px; height: auto;">            
             <h2>💜 Você é a Amethyst!</h2>
             <p>Você é divertido, espontâneo e não tem medo de ser você mesmo. 
             Adora se divertir e transformar qualquer situação em uma aventura.</p>
         `,
 
         Lapis: `
-         <img src="imagems/lapis.png" alt="Lapis Lazuli" style="width: 200px; height: auto;">
+         <img src="img/lapis.png" alt="Lapis Lazuli" style="width: 200px; height: auto;">
             <h2>💙 Você é a Lapis Lazuli!</h2>
             <p>Você é independente, sensível e valoriza muito sua liberdade. 
             Às vezes prefere ficar sozinho, mas se importa profundamente com quem ama.</p>
         `,
 
         Pearl: `
-        <img src="imagems/perola.png" alt="Pearl" style="width: 200px; height: auto;">
+        <img src="img/perola.png" alt="Pearl" style="width: 200px; height: auto;">
             <h2>🤍 Você é a Pearl!</h2>
             <p>Você é responsável, cuidadoso e perfeccionista. 
             Quando alguém precisa de ajuda, você sempre tenta encontrar a melhor solução.</p>
         `,
 
         Garnet: `
-            <img src="imagems/garnet.png" alt="Garnet" style="width: 200px; height: auto;">
+            <img src="img/garnet.png" alt="Garnet" style="width: 200px; height: auto;">
             <h2>❤️ Você é a Garnet!</h2>
             <p>Você é confiante, determinado e sabe exatamente quem é. 
             É uma pessoa forte que valoriza a confiança, o amor e a união.</p>
